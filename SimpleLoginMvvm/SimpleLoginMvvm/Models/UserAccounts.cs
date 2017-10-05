580673d04ec8c03da9a419125c7e51c30fe5aefc
Mark Deanil Vicente <markdeanilvicente@outlook.com>
2017-10-05 14:08:48 +0800

    Add project files.

---
+++ SimpleLoginMvvm/SimpleLoginMvvm/Models/UserAccounts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLoginMvvm.Models
+{
+    public class UserAccounts
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ SimpleLoginMvvm/SimpleLoginMvvm/ViewModels/LoginViewModel.cs
@@ -0,0 +1,53 @@
+﻿using SimpleLoginMvvm.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SimpleLoginMvvm.ViewModels
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        private UserAccounts userAccounts = new UserAccounts();
+
+        public UserAccounts UserAccounts
+        {
+            get { return userAccounts; }
+            set
+            {
+                userAccounts = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Command LoginCommand
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                    if (UserAccounts.Username == "admin" && UserAccounts.Password == "admin")
+                    {
+                        App.Current.MainPage.DisplayAlert("Notification", "Successfully Login", "Okay");
+                        // Open next page
+                    }
+                    else
+                    {
+                        App.Current.MainPage.DisplayAlert("Notification", "Error Login", "Okay");
+                    }
+                });
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
